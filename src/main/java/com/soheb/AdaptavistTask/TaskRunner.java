fd14e862ad99faebc7d4c8f8be9db6719c0768df
Soheb <git@sohebm.com>
2024-01-18 20:50:59 +0000

    Integrated the code into TaskRunner class

--- src/main/java/com/soheb/AdaptavistTask/TaskRunner.java
+++ src/main/java/com/soheb/AdaptavistTask/TaskRunner.java
@@ -1,8 +1,19 @@
 package com.soheb.AdaptavistTask;
 
+import com.soheb.AdaptavistTask.service.FileService;
+import com.soheb.AdaptavistTask.service.PrettyPrinterService;
+import com.soheb.AdaptavistTask.service.WordFrequencyCountService;
+import com.soheb.AdaptavistTask.service.WordFrequencyService;
+
 public class TaskRunner {
 
     public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Please provide a file that you wish to get the word frequency");
+            return;
+        }
 
+        var service = new WordFrequencyService(new FileService(), new WordFrequencyCountService(), new PrettyPrinterService());
+        service.processFile(args[0]);
     }
 }
